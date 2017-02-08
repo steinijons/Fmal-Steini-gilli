838a1c01c74b7b9814ae991370cf7e2597b467b0
steinijons <hivsteini@gmail.com>
2017-02-08 11:07:01 +0000

    lexer

--- Assignment2/Lexer.java
+++ Assignment2/Lexer.java
@@ -1,29 +1,32 @@
 package Assignment2;
 
+import Assignment2.Token.TokenCode;
+
 public class Lexer {
 
 	
-	public Token nextToken()
+	public TokenCode nextToken()
 	{
-		char input; 
+		char input = 'a'; 
 		
-		
-		switch(input)
+		if(input == '+') {return Token.TokenCode.ADD;}
+		else if(input == '-') {return Token.TokenCode.SUB;}
+		else if(input == '*') {return Token.TokenCode.MULT;}
+		else if(input == '(') {return Token.TokenCode.LPAREN;}
+		else if(input == ')') {return Token.TokenCode.RPAREN;}
+		else if(input == ';') {return Token.TokenCode.SEMICOL;}
+		else if(Character.isDigit(input)) {return Token.TokenCode.INT;}
+		else if(Character.toString(input).matches(".*[a-z]*.") || Character.toString(input).matches(".*[A-Z]*.")) 
 		{
-		case '+':
-			return();
-		case '-':
-			return();
-		case '*':
-			return();
-		case '(':
-			return();
-		case ')':
-			return();
-		case '=':
-			return();
-		case ';':
-			
-			
+			return Token.TokenCode.ID;
+		}
+		else if(input == ' ') {return Token.TokenCode.END;}
+		if(input ==  )
+		
+
+	
+		return null;
+		
 	}
+		
 }
