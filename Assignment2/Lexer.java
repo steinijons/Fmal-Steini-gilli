11b6d3465f22fafb01b07ac30212d03f62c36919
steinijons <hivsteini@gmail.com>
2017-02-08 09:49:33 +0000

    bætt ið Lexer og Token

--- Assignment2/Lexer.java
+++ Assignment2/Lexer.java
@@ -2,4 +2,28 @@ package Assignment2;
 
 public class Lexer {
 
+	
+	public Token nextToken()
+	{
+		char input; 
+		
+		
+		switch(input)
+		{
+		case '+':
+			return();
+		case '-':
+			return();
+		case '*':
+			return();
+		case '(':
+			return();
+		case ')':
+			return();
+		case '=':
+			return();
+		case ';':
+			
+			
+	}
 }
--- Assignment2/Token.java
+++ Assignment2/Token.java
@@ -1,5 +1,10 @@
 package Assignment2;
 
 public class Token {
-
+	public String lexeme;
+	public TokenCode tCode;
+	
+	enum TokenCode { ID, ASSIGN, SEMICOL, INT, ADD, SUB,
+		MULT, LPAREN, RPAREN, PRINT, END, ERROR }
+	
 }
