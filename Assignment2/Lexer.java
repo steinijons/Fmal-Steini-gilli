0029e746aa910b996f7c41493d9426cb08e38f6b
gislibg <gisligudmundss@gmail.com>
2017-02-09 23:07:19 +0000

    haha

--- Assignment2/Lexer.java
+++ Assignment2/Lexer.java
@@ -3,6 +3,8 @@ package Assignment2;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.net.ssl.SSLContext;
+
 public class Lexer {
 
 	private static Scanner scanner;
@@ -10,48 +12,132 @@ public class Lexer {
 	private ArrayList<String> words;
 	public String word;
 	String arguments; 
+	public int counter = -1;
+	private ArrayList<Character> charInput;
+	
 	public Lexer()
 	{
+		
+		charInput = new ArrayList<Character>();
+		scanner = new Scanner(System.in);
+		String arguments;
+		while(scanner.hasNextLine() && !( arguments = scanner.nextLine() ).equals( "" ))
+		{
+			for(int i = 0; i < arguments.length(); i++)
+			{
+				charInput.add(arguments.charAt(i));
+			}
+		}
+		
+		/*int i = 1;
+		for(char c: charInput)
+		{	
+			System.out.println(i + " " + c);
+			i += 1;
+		}*/
+	}
 		//System.out.println(System.in);
-		input = new ArrayList<String>();
+		/*input = new ArrayList<String>();
 		scanner = new Scanner(System.in);
 		while(scanner.hasNextLine() &&  !(arguments = scanner.nextLine() ).equals(""))
 		{
 			input.add(arguments); 
+			for(int i = 0; i < input.size(); i++)
+			{
+				System.out.println(input.get(i));
+			}
 		}
+		
 		words = new ArrayList<String>();
 		for(String arg : input)
 		{
-			String[] temp = arg.split("(\\s+)|((?<=;)|(?=;))");
+			String[] temp = arg.split("\\s+");
 			for(String s : temp)
 			{
-				words.add(s);
+				if(s.contains(";") || s.contains("(") || s.contains("-") || s.contains(")"))
+				{
+					String[] numb = s.split(";");
+					System.out.println(numb[0]);
+					words.add(numb[0]);
+					words.add(";");
+				}
+				else
+				{
+					words.add(s);
+				}
+				
 			}
+			
 		}
-		System.out.print(words);
-		System.out.println("");
-	}
+		System.out.println(words);
+	}*/
 	
 	public Token nextToken()
 	{   
-		word = words.get(0); 
-		//System.out.println(word + " size:" + word.length());
-		words.remove(0);
-		if(word.equals("+")) {return new Token(Token.TokenCode.ADD, word);}
-		else if(word.equals("-")) {return new Token(Token.TokenCode.SUB, word);}
-		else if(word.equals("*")) {return new Token(Token.TokenCode.MULT, word);}
-		else if(word.equals("(")) {return new Token(Token.TokenCode.LPAREN, word);}
-		else if(word.equals(")")) {return new Token(Token.TokenCode.RPAREN, word);}
-		else if(word.equals(";")) {return new Token(Token.TokenCode.SEMICOL, word);}
-		else if(word.equals("=")) {return new Token(Token.TokenCode.ASSIGN, word);}
-		
-		//System.out.println(word)
-		if(word.matches(".*[a-z].*") || word.matches(".*[A-Z].*"))
+		counter++;
+		if(Character.isWhitespace(charInput.get(counter)))
 		{
-			return new Token(Token.TokenCode.ID, word);
+			//System.out.println("WHITESPACE");				
+			nextToken();
 		}
-		else if(word.matches("[0-9]+")) 
+		
+		String string = "";
+		word = charInput.get(counter).toString();
+		//System.out.println("Current Element: " + word);
+		
+		if(Character.isLetter(charInput.get(counter)))
 		{
+			//System.out.println("Fer inní isletter");
+			while(true)
+			{
+				if(Character.isLetter(charInput.get(counter)))
+				{
+					string += charInput.get(counter).toString();
+				}
+				else
+				{
+					break;
+				}		
+				counter++;
+			}
+			if(Character.isWhitespace(charInput.get(counter)) || charInput.get(counter).equals(')'))
+			{
+				return new Token(Token.TokenCode.ID, string);
+			}
+			else if(string.equals("print"))
+			{
+				return new Token(Token.TokenCode.PRINT, string);
+			}
+			else if(string.equals("end"))
+			{
+				return new Token(Token.TokenCode.END, string);
+			}		
+		}
+		else
+		{
+			if(word.equals("+")) {return new Token(Token.TokenCode.ADD, word);}
+			else if(word.equals("-")) {return new Token(Token.TokenCode.SUB, word);}
+			else if(word.equals("*")) {return new Token(Token.TokenCode.MULT, word);}
+			else if(word.equals("(")) {return new Token(Token.TokenCode.LPAREN, word);}
+			else if(word.equals(")")) {return new Token(Token.TokenCode.RPAREN, word);}
+			else if(word.equals(";")) {return new Token(Token.TokenCode.SEMICOL, word);}
+			else if(word.equals("=")) {return new Token(Token.TokenCode.ASSIGN, word);}
+			else if(isInteger(word))
+			{			
+				return new Token(Token.TokenCode.INT, word);
+			}
+		}
+		//word = input.get(0);
+		//input.remove(0);
+		//splita word svo hægt sé að gefa viðeigandi token :)
+		
+		
+		
+		
+		   
+		//System.out.println(word);
+		/*if(isInteger(word))
+		{			
 			return new Token(Token.TokenCode.INT, word);
 		}
 		else if(word == "print")
@@ -66,12 +152,22 @@ public class Lexer {
 		{
 			return new Token(Token.TokenCode.ID, word);
 		}			
-		
-		return new Token(Token.TokenCode.SEMICOL, word);	
-	}				
+		*/
+		return new Token(Token.TokenCode.ERROR, word);	
+	}	
+	
+	public boolean isInteger(String input)
+	{
+	   try
+	   {
+	      Integer.parseInt(input);
+	      
+	   }
+	   catch(NumberFormatException nfe)
+	   {
+	      return false;
+	   }
+	   return true;
+	}
 		
 }
-		
-
-
-		
--- Assignment2/Parser.java
+++ Assignment2/Parser.java
@@ -14,7 +14,6 @@ public class Parser {
 	public void parse() throws IOException 
 	{
 		nextToken = lex.nextToken();
-		System.out.println(nextToken.tCode);
 		statements();
 	}
 	
@@ -22,6 +21,7 @@ public class Parser {
 	
 	public void statements() 
 	{
+		//System.out.println("statements()");
 		if(nextToken.tCode == Token.TokenCode.END)
 		{
 			return;
@@ -31,14 +31,17 @@ public class Parser {
 			statement();
 			if(nextToken.tCode == Token.TokenCode.SEMICOL)
 			{
+				//System.out.println("SEMICOL");
 				nextToken = lex.nextToken();
 				statements();
 			}
 			else
 			{
+				System.out.println("SEMICOLERROR");
 				error();
 			}
 		}
+		System.out.println("statemenst()ERROR");
 		error();
 		
 	}
@@ -46,38 +49,45 @@ public class Parser {
 	
 	public void statement() 
 	{
+		//System.out.println("statement()");
 		if(nextToken.tCode == Token.TokenCode.ID)
 		{
-			//System.out.println(nextToken.lexeme);
+			//System.out.println("INNÍ ID");
+			System.out.println("PUSH " + nextToken.lexeme);
 			nextToken = lex.nextToken();
-			System.out.println(nextToken.tCode);
 			if(nextToken.tCode == Token.TokenCode.ASSIGN)
 			{
-				//System.out.println("ASSIGN");
+				//System.out.println("INNÍ ASSIGN");
 				nextToken = lex.nextToken();
-				expr();				
+				expr();
+				//System.out.println("ASSIGN");
 			}
 			else
 			{
+				//System.out.println("ASSIGNMENT ERROR");
 				error();
 			}
 		}
 		else if(nextToken.tCode == Token.TokenCode.PRINT)
 		{
+			//System.out.println("INNÍ PRINT");
 			nextToken = lex.nextToken();
-			System.out.println("PRINT");
+			//System.out.println("PRINT");
 			if(nextToken.tCode == Token.TokenCode.ID)
 			{
-				//System.out.println(nextToken.lexeme);
+				//System.out.println("INNÍ PRINT ID");
+				System.out.println("PUSH" + nextToken.lexeme);
 				nextToken = lex.nextToken();
 			}
 			else
 			{
+				//System.out.println("PRINT EKKI ID ERROR");
 				error();
 			}
 		}
 		else
 		{
+			//System.out.println("STATEMENT ERROR");
 			error();
 		}
 	}
@@ -86,22 +96,21 @@ public class Parser {
 	
 	public void expr()
 	{
+		//System.out.println("expr()");
 		term();
 		if(nextToken.tCode == Token.TokenCode.ADD)
 		{
+			//System.out.println("INNÍ ADD");
 			nextToken = lex.nextToken();
 			expr();
-			System.out.println("ADD");
+			//System.out.println("ADD");
 		}
 		else if(nextToken.tCode == Token.TokenCode.SUB)
 		{
+			//System.out.println("INNÍ SUB");
 			nextToken = lex.nextToken();
 			expr();
-			System.out.println("ADD");
-		}
-		else
-		{
-			error();
+			//System.out.println("SUB");
 		}
 	}
 	
@@ -109,48 +118,61 @@ public class Parser {
 	
 	public void term()
 	{
+		//System.out.println("TERM()");
 		factor();
 		if(nextToken.tCode == Token.TokenCode.MULT)
 		{
+			//System.out.println("INNÍ MULT");
 			nextToken = lex.nextToken();
 			term();
-			System.out.println("MULT");
+			//System.out.println("MULT");
 		}
 	}
 	
 	//int | id | ( Expr )
 	public void factor()
 	{
+		//System.out.println("FACTOR()");
 		if(nextToken.tCode == Token.TokenCode.INT)
 		{
-			System.out.println("PUSH" + nextToken.lexeme);
+			//System.out.println("FACTOR() INNÍ INT");
+			//System.out.println("PUSH " + nextToken.lexeme);
 			nextToken = lex.nextToken();
-		}
+		}		
 		else if(nextToken.tCode == Token.TokenCode.ID)
 		{
-			System.out.println("PUSH" + nextToken.lexeme);
+			//System.out.println("FACTOR() INNÍ INT");
+			//System.out.println("PUSH " + nextToken.lexeme);
+			nextToken = lex.nextToken();
 		}
 		else if(nextToken.tCode == Token.TokenCode.LPAREN)
 		{
-			expr();
+			//System.out.println("FACTOR() INNÍ LPAREN");
 			nextToken = lex.nextToken();
+			expr();
+			//System.out.println("HÉR ER ÉG!!!!!!!");
+			//System.out.println(nextToken.lexeme);
+			
 			if(nextToken.tCode == Token.TokenCode.RPAREN)
 			{
+				//System.out.println("FACTOR() INNÍ RPAREN");
 				nextToken = lex.nextToken();
 			}
 			else
 			{
+				//System.out.println("FACTOR() EKKI LPAREN NÉ RPAREN");
 				error();
 			}
 		}
 		else
 		{
+			//System.out.println("FACTOR() ERROR");
 			error();
 		}
 	}
 	
 	public void error()
 	{
-		System.out.println("ERROR!");
+		System.out.println("Syntax error!");
 	}
 }
