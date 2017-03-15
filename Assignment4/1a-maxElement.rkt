4cad71d9bd69528aefcd29ad700ab03891bdf998
Steini <hivsteini@gmail.com>
2017-03-15 21:27:46 +0000

    einfaldað maxElement

--- Assignment4/1a-maxElement.rkt
+++ Assignment4/1a-maxElement.rkt
@@ -2,8 +2,5 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname 1a-maxElement) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (define (maxElement lis)
-    (cond 
-       ((null? (cdr lis)) '())
-       (else (max (car lis) (maxElement (cdr lis))))
-    )
+	(apply max lis)
 )
